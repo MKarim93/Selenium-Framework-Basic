703abcdd5a9541454ec824b36f33b1f2c9ea9bbf
Mohammad Karim <mohammadekarim14@gmail.com>
2025-07-23 20:09:15 -0400

    Added isURlDisplayed Method for ElementsPage class

--- src/main/java/demoqa/pages/ElementsPage.java
+++ src/main/java/demoqa/pages/ElementsPage.java
@@ -1,8 +1,14 @@
 package demoqa.pages;
 
 import base.BasePage;
+import org.openqa.selenium.By;
 
 public class ElementsPage extends BasePage {
 
+    private String elementsPageURL = "https://demoqa.com/elements";
 
+    public boolean isElementsPageURLDisplayed(){
+        String currentURL = driver.getCurrentUrl();
+        return currentURL.equals(elementsPageURL);
+    }
 }
